a28170a21a314c55e94ee1a586c3ec63a6878744
XLuoChen <1425145741@qq.com>
2016-07-18 17:18:36 +0800

    realize buildCartItems()

--- pos_v1/main/main.js
+++ pos_v1/main/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-//TODO: 请在该文件中实现练习要求并删除此注释
+var buildCartItems = (tags, allItems) => {
+  let cartItems = [];
+
+  for (let tag of tags) {
+    let tagArray = tag.split('-');
+    let barcode = tagArray[0];
+
+    let count = parseFloat(tagArray[1] || 1);
+
+    var cartItem = cartItems.find(cartItem => cartItem.item.barcode === barcode);
+    if (cartItem != null) {
+      cartItem.count++;
+    }
+    else {
+      let item = allItems.find(item => item.barcode === barcode);
+      cartItems.push({item: item, count: count});
+    }
+  }
+
+  return cartItems;
+};
+
