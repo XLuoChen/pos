da46bec4fddd972d8265199673e309c60a37c620
XLuoChen <1425145741@qq.com>
2016-07-18 17:39:50 +0800

    change var to let

--- pos_v1/main/test.js
+++ pos_v1/main/test.js
@@ -1,24 +1,19 @@
-'use strict';
-
 let buildCartItems = (tags, allItems) => {
-  let cartItems = [];
-
-  for (let tag of tags) {
+  const cartItems = [];
+  
+  for (let tag of tags){
     let tagArray = tag.split('-');
     let barcode = tagArray[0];
-
     let count = parseFloat(tagArray[1] || 1);
-
-    var cartItem = cartItems.find(cartItem => cartItem.item.barcode === barcode);
-    if (cartItem) {
-      cartItem.count++;
+    
+    let cartItem = cartItems.find(cartItem => cartItem.item.barcode === barcode);
+    if (cartItem){
+      cartItem.count ++;
     }
     else {
       let item = allItems.find(item => item.barcode === barcode);
-      cartItems.push({item: item, count: count});
+      cartItems.push({item:item,count:count});
     }
   }
-
   return cartItems;
 };
-
