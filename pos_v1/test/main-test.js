840212e11f12ff54dfc854d1cd3e2411127b6725
XLuoChen <1425145741@qq.com>
2016-07-19 19:47:35 +0800

    realize all tasks test

--- pos_v1/test/main-test.js
+++ pos_v1/test/main-test.js
@@ -53,7 +53,7 @@ describe('pos', () => {
     expect(cartItems).toEqual(expectCartItems);
   });
 
-  it('should buildReceiptItems', () => {
+  it('should build receiptItems', () => {
 
     let cartItems = buildCartItems(inputs, loadAllItems());
 
@@ -102,6 +102,80 @@ describe('pos', () => {
     let receiptItems = buildReceiptItems(cartItems, loadPromotions());
 
     expect(receiptItems).toEqual(expectReceiptItems);
+  });
+
+  it('should build receipt', () => {
+
+    let cartItems = buildCartItems(inputs, loadAllItems());
+    let receiptItems = buildReceiptItems(cartItems, loadPromotions());
+    let receipt = buildReceipt(receiptItems);
+
+    let expectReceipt = {
+      receiptItems: [
+        {
+          cartItem: {
+            item: {
+              barcode: 'ITEM000001',
+              name: '雪碧',
+              unit: '瓶',
+              price: 3.00
+            },
+            count: 5
+          },
+          subtotal: 12.00,
+          saved: 3.00
+        },
+        {
+          cartItem: {
+            item: {
+              barcode: 'ITEM000003',
+              name: '荔枝',
+              unit: '斤',
+              price: 15.00
+            },
+            count: 2
+          },
+          subtotal: 30.00,
+          saved: 0
+        },
+        {
+          cartItem: {
+            item: {
+              barcode: 'ITEM000005',
+              name: '方便面',
+              unit: '袋',
+              price: 4.50
+            },
+            count: 3
+          },
+          subtotal: 9.00,
+          saved: 4.50
+        }
+      ],
+      total: 51.00,
+      savedTotal: 7.50
+    };
+
+    expect(receipt).toEqual(expectReceipt);
+  });
+
+  it('it should build receiptText', () => {
+
+    let cartItems = buildCartItems(inputs, loadAllItems());
+    let receiptItems = buildReceiptItems(cartItems, loadPromotions());
+    let receipt = buildReceipt(receiptItems);
+
+    let receiptText = buildReceiptText(receipt);
+    let expectReceiptText = `***<没钱赚商店>收据***
+名称：雪碧，数量：5瓶，单价：3.00(元)，小计：12.00(元)
+名称：荔枝，数量：2斤，单价：15.00(元)，小计：30.00(元)
+名称：方便面，数量：3袋，单价：4.50(元)，小计：9.00(元)
+----------------------
+总计：51.00(元)
+节省：7.50(元)
+**********************`;
+
+    expect(receiptText).toEqual(expectReceiptText);
   });
 
   it('should print correct text', () => {
