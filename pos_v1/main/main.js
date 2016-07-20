37891d2a6f92d5ae296655d789da5f54b5f88e31
XLuoChen <1425145741@qq.com>
2016-07-20 11:53:02 +0800

    optimization

--- pos_v1/main/main.js
+++ pos_v1/main/main.js
@@ -10,9 +10,7 @@ let printReceipt = (tags) => {
 
   let receipt = buildReceipt(receiptItems);
 
-  let receiptText = buildReceiptText(receipt);
-
-  console.log(receiptText);
+  console.log( buildReceiptText(receipt));
 };
 
 let buildCartItems = (tags, allItems) => {
@@ -84,26 +82,27 @@ let buildReceipt = (receiptItems) => {
 
 let buildReceiptText = (receipt) => {
 
-  return `***<没钱赚商店>收据***${generateItemText(receipt.receiptItems)}
+  return `***<没钱赚商店>收据***
+${generateItemsText(receipt.receiptItems)}
 ----------------------
 总计：${priceToFixed(receipt.total)}(元)
 节省：${priceToFixed(receipt.savedTotal)}(元)
 **********************`
 };
 
-let generateItemText = (receiptItems) => {
+let generateItemsText = (receiptItems) => {
 
-  let itemText = '';
+  let itemsText = receiptItems.map(receiptItem => {
 
-  for (let receiptItem of receiptItems) {
     let item = receiptItem.cartItem.item;
+    return `名称：${item.name}，\
+数量：${fixedMoney.cartItem.count}${item.unit}，\
+单价：${fixedMoney(item.price)}(元)，\
+小计：${fixedMoney(receiptItem.subtotal)}(元)`;}).join('\n');
 
-    itemText +=
-      `\n名称：${item.name}，数量：${receiptItem.cartItem.count}${item.unit}，单价：${priceToFixed(item.price)}(元)，小计：${priceToFixed(receiptItem.subtotal)}(元)`;
-  }
-  return itemText;
+  return itemsText;
 };
 
-let priceToFixed = (price) => {
-  return price.toFixed(2);
+let fixedMoney = (money) => {
+  return money.toFixed(2);
 };
--- pos_v1/test/main-test.js
+++ pos_v1/test/main-test.js
@@ -159,25 +159,6 @@ describe('pos', () => {
     expect(receipt).toEqual(expectReceipt);
   });
 
-  it('it should build receiptText', () => {
-
-    let cartItems = buildCartItems(inputs, loadAllItems());
-    let receiptItems = buildReceiptItems(cartItems, loadPromotions());
-    let receipt = buildReceipt(receiptItems);
-
-    let receiptText = buildReceiptText(receipt);
-    let expectReceiptText = `***<没钱赚商店>收据***
-名称：雪碧，数量：5瓶，单价：3.00(元)，小计：12.00(元)
-名称：荔枝，数量：2斤，单价：15.00(元)，小计：30.00(元)
-名称：方便面，数量：3袋，单价：4.50(元)，小计：9.00(元)
-----------------------
-总计：51.00(元)
-节省：7.50(元)
-**********************`;
-
-    expect(receiptText).toEqual(expectReceiptText);
-  });
-
   it('should print correct text', () => {
 
     spyOn(console, 'log');
